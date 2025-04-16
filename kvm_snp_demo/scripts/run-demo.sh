93735d43bb15fe24eeb194d4a9af1b2e5ad95001
Alex Orozco <alexorozco@google.com>
2025-04-16 23:40:27 +0000

    Remove seccomp=false
    We added the statx syscall that Oak Stage0 was using to read files from fw_cfg to the vcpu seccomp filter so now we don't need to disable the filter anymore

--- kvm_snp_demo/scripts/run-demo.sh
+++ kvm_snp_demo/scripts/run-demo.sh
@@ -10,7 +10,7 @@ run_demo() {
             --disk path=disks/focal-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
             --cmdline "console=ttyS0 panic=-1 brd.rd_nr=1 brd.rd_size=10485760 brd.max_part=1 \
             ip=192.168.111.12::192.168.111.11:255.255.255.0::enp0s1:off root=/dev/vda1 rw" \
-            --seccomp false --serial file=chv-serial.log --log-file ch-fw.log -vvv;
+            --serial file=chv-serial.log --log-file ch-fw.log -vvv;
     else 
         ./cloud-hypervisor --cpus boot=4 --memory size=8000000K \
             --net tap=tap1234,ip=192.168.111.12,mac=12:34:56:78:90:ab,mask=255.255.255.0 \
@@ -18,7 +18,7 @@ run_demo() {
             --disk path=disks/focal-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
             --cmdline "console=ttyS0 panic=-1 brd.rd_nr=1 brd.rd_size=10485760 brd.max_part=1 \
             ip=192.168.111.12::192.168.111.11:255.255.255.0::enp0s1:off root=/dev/vda1 rw" \
-            --seccomp false --serial file=chv-serial.log --log-file ch-fw.log -vvv;
+            --serial file=chv-serial.log --log-file ch-fw.log -vvv;
     fi 
 }
 
