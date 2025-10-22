6d51761ce2fbabc9bf938f6898ce5a0aac48f663
Alex Orozco <alexorozco@google.com>
2025-10-22 19:54:20 +0000

    fix: Update disk path in run-demo script
    Update from focal->noble image (already updated build-lib.sh)

--- kvm_snp_demo/scripts/run-demo.sh
+++ kvm_snp_demo/scripts/run-demo.sh
@@ -7,7 +7,7 @@ run_demo() {
         ./cloud-hypervisor --cpus boot=4 --memory size=8000000K --platform sev_snp=true \
             --net tap=tap1234,ip=192.168.111.12,mac=12:34:56:78:90:ab,mask=255.255.255.0 \
             --vsock cid=3,socket=/tmp/ch.vsock --igvm firmware/stage0.igvm --kernel kernels/kernel_bin \
-            --disk path=disks/focal-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
+            --disk path=disks/noble-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
             --cmdline "console=ttyS0 panic=-1 brd.rd_nr=1 brd.rd_size=10485760 brd.max_part=1 \
             ip=192.168.111.12::192.168.111.11:255.255.255.0::enp0s1:off root=/dev/vda1 rw" \
             --fw-cfg-config initramfs=off --serial file=chv-serial.log --log-file ch-fw.log -vvv;
@@ -15,7 +15,7 @@ run_demo() {
         ./cloud-hypervisor --cpus boot=4 --memory size=8000000K \
             --net tap=tap1234,ip=192.168.111.12,mac=12:34:56:78:90:ab,mask=255.255.255.0 \
             --vsock cid=3,socket=/tmp/ch.vsock --igvm firmware/stage0_nosnp.igvm --kernel kernels/kernel_bin \
-            --disk path=disks/focal-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
+            --disk path=disks/noble-server-cloudimg-amd64.raw path=disks/ubuntu-cloudinit.img  \
             --cmdline "console=ttyS0 panic=-1 brd.rd_nr=1 brd.rd_size=10485760 brd.max_part=1 \
             ip=192.168.111.12::192.168.111.11:255.255.255.0::enp0s1:off root=/dev/vda1 rw" \
             --fw-cfg-config initramfs=off --serial file=chv-serial.log --log-file ch-fw.log -vvv;
